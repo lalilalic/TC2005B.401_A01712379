b48939ef6a630f46378fa3ca461420846d8c25b6
edjuarezp <edjuarezp@L01452763M01.local>
2026-02-23 12:02:07 -0600

    cleanup

--- app.js
+++ app.js
@@ -1,20 +1,4 @@
-console.log("hola desde node!");
-
-const filesystem = require('fs');
-
-filesystem.writeFileSync('hola.txt', 'Hola desde node');
-
-setTimeout(() => {
-    console.log("jojo te hackié!");
-}, 15000);
-
-const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
-
-for (let item of arreglo) {
-    setTimeout(() => {
-        console.log(item);
-    }, item);
-} 
+const http = require('http');
 
 const html = `
 <!DOCTYPE html>
@@ -119,18 +103,14 @@ const html = `
 </html>
 `;
 
-const http = require('http');
-
 const server = http.createServer((request, response) => {  
-//    console.log(request);  
-//    console.log(response);
+
     console.log(request.url);
     response.setHeader('Content-Type', 'text/html');
     response.write(html);
     response.end();
 
-        const datos_completos = [];
-
+    //const datos_completos = [];
 
     // request.on('data', (data) => {
     //   console.log(data);
