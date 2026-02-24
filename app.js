318e22871b008f4ab642581826a6e5e177eaf480
edjuarezp <edjuarezp@L01452763M01.local>
2026-02-24 12:57:18 -0600

    crear respuesta 404 para las rutas que no están definidas

--- app.js
+++ app.js
@@ -7,4 +7,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 const rutas_personajes = require('./routes/personajes.routes');
 app.use('/personajes', rutas_personajes);
 
+app.use((request, response, next) => {
+    response.status(404).send("La ruta no existe");
+})
+
 app.listen(3000);
