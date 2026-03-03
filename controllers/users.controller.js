db9ccbe46bd3a0c086901a408d9ff6c7f0a81626
edjuarezp <edjuarezp@L01452763M01.local>
2026-03-03 12:52:46 -0600

    crear forma de signup

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -14,3 +14,21 @@ exports.get_logout = (request, response, next) => {
         response.redirect('/users/login'); //Este código se ejecuta cuando la sesión se elimina.
     });
 };
+
+exports.get_signup = (request, response, next) => {
+    const error = request.session.error || '';
+    request.session.error = '';
+    response.render('signup', {
+        username: request.session.username || '',
+        error: error,
+    })
+};
+
+exports.post_signup = (request, response, next) => {
+    if (request.body.password != request.body.password_confirm) {
+        request.session.error = 'Las contraseñas no coinciden';
+        return response.redirect('/users/signup');
+    } else {
+        return response.redirect('/users/login');
+    }
+};
