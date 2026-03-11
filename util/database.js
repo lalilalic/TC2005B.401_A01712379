2018fcba36bf5279af189934c244199832103b8d
Laura Cintora <A01712379>
2026-03-10 21:32:28 -0600

    Polish lab 18 UI and database config

--- util/database.js
+++ util/database.js
@@ -3,8 +3,9 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    database: 'lol',
+    database: 'lol18',
     password: '',
 });
 
 module.exports = pool.promise();
+
