4b138ac4a7907f55c84323c565007a26f45b27b2
Laura Cintora <A01712379>
2026-04-19 20:42:46 -0600

    Creacion de ruta para bsucar los destinos de manera asincrona

--- Lab24/routes/destinoss.routes.js
+++ Lab24/routes/destinoss.routes.js
@@ -5,6 +5,8 @@ const canCreate = require('../util/can-create');
 const canView = require('../util/can-view');
 const destinosController = require('../controllers/destinos.controllers');
 
+// Ruta para buscar destinos de forma asíncrona
+router.get('/buscar/:busqueda', isAuth, canView, destinosController.get_buscar);
 router.get('/new', isAuth, canCreate, destinosController.get_add);
 router.get('/add', isAuth, canCreate, destinosController.get_add);
 router.get('/nuevo', isAuth, canCreate, destinosController.get_add);
@@ -13,5 +15,7 @@ router.get('/:destino_id/edit', isAuth, canCreate, destinosController.get_edit);
 router.post('/edit', isAuth, canCreate, destinosController.post_edit);
 router.use('/:destino_id', isAuth, canView, destinosController.get_list);
 router.use(isAuth, canView, destinosController.get_list);
+
+
 
 module.exports = router;
